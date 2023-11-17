b4bf9417760971fa07347655a8f1a6cf7c7bd7db
Semih <semih.turkoglu@student.vinci.be>
2023-11-17 10:21:58 +0100

    week9: refactor

--- AJ_atelier09/src/main/java/be/vinci/aj/Main.java
+++ AJ_atelier09/src/main/java/be/vinci/aj/Main.java
@@ -1,11 +1,12 @@
 package be.vinci.aj;
 
+import be.vinci.aj.domaine.QueryFactoryImpl;
 import be.vinci.aj.server.ProxyServer;
 
 
 public class Main {
     public static void main(String[] args) {
-        ProxyServer proxy = new ProxyServer();
+        ProxyServer proxy = new ProxyServer(new QueryFactoryImpl());
         proxy.startServer();
     }
 }
--- AJ_atelier09/src/main/java/be/vinci/aj/server/ProxyServer.java
+++ AJ_atelier09/src/main/java/be/vinci/aj/server/ProxyServer.java
@@ -2,10 +2,15 @@ package be.vinci.aj.server;
 
 import be.vinci.aj.domaine.Query;
 import be.vinci.aj.domaine.QueryFactory;
-
 import java.util.Scanner;
 
 public class ProxyServer {
+
+    private QueryFactory queryFactory;
+
+    public ProxyServer(QueryFactory queryFactory){
+        this.queryFactory = queryFactory;
+    }
     public void startServer() {
 
         try (Scanner scan = new Scanner(System.in)) {
